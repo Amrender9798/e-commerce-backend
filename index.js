2f28ca91bef1c8ad59ce94d66d797c74a6893119
Amrender9798 <64745853+Amrender9798@users.noreply.github.com>
2024-03-03 01:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
-import express from 'express';
-import http from 'http';
-import dotenv from 'dotenv';
-import authRoutes from './routes/authRoutes.js';
-import connectToDatabase from './db/connection.js';
+import express from "express";
+import http from "http";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import connectToDatabase from "./db/connection.js";
+import authRouter from "./routes/authRoutes.js";
+import cartRouter from "./routes/cartRoutes.js";
+import authenticate from "./middlewares/authenticate.js";
+import productRouter from "./routes/productRoutes.js";
+import orderRouter from "./routes/orderRoutes.js";
+import reviewRouter from "./routes/authRoutes.js";
+import personalInfoRouter from "./routes/personalInfoRoutes.js";
 
 dotenv.config();
+
 const app = express();
 const server = http.createServer(app);
 
+app.use(cors());
 app.use(express.json());
-app.use('/auth',authRoutes);
 
+const uploadsPath = path.join("uploads");
+app.use("/uploads", express.static(uploadsPath));
+
+app.use("/auth", authRouter);
+app.use("/products", productRouter);
+app.use("/cart", authenticate, cartRouter);
+app.use("/reviews", reviewRouter);
+app.use("/orders", authenticate, orderRouter);
+app.use("/personal-info",authenticate,personalInfoRouter);
 
 const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
   connectToDatabase();
-  
 });
